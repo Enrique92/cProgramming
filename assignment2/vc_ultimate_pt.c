faa9a8f7a935d905183bb85fc4bcd8bfa3101a44
Enrique <enrique.alcaide92@gmail.com>
2019-02-06 20:36:05 -0800

    Update excercises Assigment2

--- assignment2/vc_ultimate_pt.c
+++ assignment2/vc_ultimate_pt.c
@@ -8,11 +8,23 @@
 
 void vc_ultimate_pt(int *********n)
 {
-    *n = 77;
+    *********n = 77;
 }
 
 int main()
 {
-    printf(vc_ultimate_pt(77));
-    return 0;
+   int a = 0;
+   int *a1 = &a;
+   int *a2 = &a1;
+   int *a3 = &a2;
+   int *a4 = &a3;
+   int *a5 = &a4;
+   int *a6 = &a5;
+   int *a7 = &a6;
+   int *a8 = &a7;
+   int *a9 = &a8;
+   printf("%d , \n",a);
+   vc_ultimate_pt(a9);
+   printf("%d",a);
+   return 0;
 }
