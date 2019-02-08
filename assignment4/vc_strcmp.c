fa57e80de6ce37ee54d8ded58504b0145e4d1d72
Enrique <enrique.alcaide92@gmail.com>
2019-02-08 09:22:53 -0800

    Update assignment4 excercises

--- assignment4/vc_strcmp.c
+++ assignment4/vc_strcmp.c
@@ -1,7 +1,14 @@
 /**
- * File              : vc_print_alphabet.c
+ * File              : vc_strcmp.c
  * Author            : Daisuke and Enrique
  * Date              : Thu 07 Jan 2019
  */
 
-#include <stdio.h>
+int vc_strcmp(char *s1, char *s2) {
+  for (; *s1 && *s2; s1++, s2++) {
+    if (*s1 - *s2) {
+      return *s1 - *s2;
+    }
+  }
+  return *s1 - *s2;
+}
